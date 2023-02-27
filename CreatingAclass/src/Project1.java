89fad3f7ebb2965404220dac997f0372f49a5136
TMahlaule <tmgaffer@gmail.com>
2023-02-27 18:51:30 +0200

    first commit

---
+++ CreatingAclass/src/Project1.java
@@ -0,0 +1,31 @@
+public class Project1 {
+
+public  static  void main(String arg [])
+{
+DisplayName();
+//Add2Numbers();
+
+    Project1 T = new Project1();
+    T.Add2Numbers();
+
+}
+
+public static void DisplayName()
+{
+
+    System.out.println("Tinyiko Mahlaule");
+
+}
+
+public void Add2Numbers()
+{
+    int  a = 10;
+    int  b = 35;
+
+ int c = a * b;
+
+ System.out.println("total " + c);
+
+}
+
+}
